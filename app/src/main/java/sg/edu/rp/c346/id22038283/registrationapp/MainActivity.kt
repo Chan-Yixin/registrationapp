05062459e8ba29ef60f5877c3006d90c65050e42
CHAN YIXIN <22038283@myrp.edu.sg>
2023-05-02 15:01:21 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22038283/registrationapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package sg.edu.rp.c346.id22038283.registrationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
